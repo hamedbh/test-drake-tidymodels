aa737641f6d1c29d92e2d5da85eafc181aa65bc7
hamedbh <hamedbh@users.noreply.github.com>
2020-03-19 20:04:04 +0000

    load all function files by pattern

--- _drake.R
+++ _drake.R
@@ -1,12 +1,9 @@
 source(here::here("R/packages.R"))
 
-source(here::here("R/f_general.R"))
-source(here::here("R/f_data_gathering.R"))
-source(here::here("R/f_plotting.R"))
-source(here::here("R/f_data_prep.R"))
-source(here::here("R/f_ranger.R"))
-source(here::here("R/f_mlp.R"))
-source(here::here("R/f_xgboost.R"))
+list.files(here::here("R"), 
+           pattern = "^f", 
+           full.names = TRUE) %>% 
+    walk(source)
 
 source(here::here("R/f_evaluate_models.R"))
 
