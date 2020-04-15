ee4d4377d8ecf6cfa9c53ff5f9ef84615739da7b
hamedbh <hamedbh@users.noreply.github.com>
2020-04-15 09:25:33 +0100

    Tidy up the plan, show sections with comments, rename objects for consistency with tidymodels standards

--- R/plan.R
+++ R/plan.R
@@ -7,32 +7,39 @@ g_plan <- drake_plan(
     training_split = partition_data(d = clean_df,
                                     p = 0.8,
                                     RNG_seed = 943),
-    barplot_grid = plot_barplot_grid(training_split %>% training()),
-    density_plot_grid = plot_density_grid(training_split %>% training()),
     g_train = training(training_split),
     g_test = testing(training_split),
     g_folds = create_cv_folds(g_train, 1738), 
-    g_recipe = create_g_pre_proc(g_train),
+    
+    # Plot objects
+    barplot_grid = plot_barplot_grid(training_split %>% training()),
+    density_plot_grid = plot_density_grid(training_split %>% training()),
+    
+    # XGBoost objects
+    xgb_rec = create_xgb_pre_proc(g_train),
+    xgb_mod = define_xgb(trees = 100, 
+                         learn_rate = 0.05),
+    xgb_wfl = create_xgb_wflow(xgb_rec, xgb_mod),
+    xgb_params = create_xgb_params(xgb_wfl, xgb_rec),
+    xgb_grid = create_xgb_grid(xgb_params, 10, 1645), 
+    xgb_grid_tune = tune_xgb_grid(
+        wflow = xgb_wfl,
+        resamples = g_folds,
+        grid = xgb_grid,
+        params = xgb_params
+        ),
+    xgb_bayes_tune = tune_xgb_bayes(
+        wflow = xgb_wfl,
+        resamples = g_folds,
+        iter = 20L,
+        params = xgb_params,
+        grid_results = xgb_grid_tune)
+    
+    
     # ranger_spec = define_ranger(500, 725),
     # ranger_grid = create_ranger_grid(g_train, 5, 737),
     # ranger_CV = run_ranger_CV(ranger_spec, ranger_grid, cv_folds),
     # ranger_CV_metrics = get_CV_metrics(ranger_CV), 
-    xgb_mod = define_xgb(10, 0.3),
-    xgb_wflow = create_xgb_wflow(g_recipe, xgb_mod),
-    xgb_params = create_xgb_params(xgb_wflow, g_recipe),
-    # xgb_grid = create_xgb_grid(params = xgb_params,
-    #                            size = 10,
-    #                            RNG_seed = 2057),
-    # xgb_grid_tune = tune_xgb_grid(
-    #     wflow = xgb_wflow,
-    #     resamples = boots,
-    #     params = xgb_params,
-    #     grid = xgb_grid),
-    # xgb_bayes_tune = tune_xgb_bayes(
-    #     wflow = xgb_wflow,
-    #     resamples = boots,
-    #     params = xgb_params,
-    #     grid_results = xgb_grid_tune,
-    #     iter = 1L,
-    #     RNG_seed = 700)
+    
+    
 )
