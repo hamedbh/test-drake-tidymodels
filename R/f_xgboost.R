946e737fc582f20250f1054f4cc9245540b416dc
hamedbh <hamedbh@users.noreply.github.com>
2020-04-15 09:42:21 +0100

    Fixed the bug for XGBoost, use sample_prop() to get a double rather than integer value for sample_size

--- R/f_xgboost.R
+++ R/f_xgboost.R
@@ -25,7 +25,7 @@ define_xgb <- function(trees, learn_rate) {
 }
 
 create_xgb_wflow <- function(pre_proc, model_spec) {
-    workflow() %>%
+    workflows::workflow() %>%
         add_model(spec = model_spec) %>%
         add_recipe(recipe = pre_proc)
 }
@@ -34,55 +34,61 @@ create_xgb_params <- function(wflow,
                               pre_proc) {
     wflow %>%
         parameters() %>%
-        finalize(pre_proc %>% 
-                     prep() %>% 
-                     juice() %>% 
-                     select(-outcome))
+        update(mtry = mtry(range = c(1L,
+                                     ncol(pre_proc %>%
+                                              prep() %>%
+                                              juice() %>%
+                                              select(-outcome)
+                                     ))), 
+               sample_size = sample_prop(c(0.4, 1))
+        )
+    # finalize(pre_proc %>% 
+    #              prep() %>%
+    #              juice() %>%
+    #              select(-outcome))
 }
 
-# create_xgb_grid <- function(params, 
-#                             size, 
-#                             RNG_seed) {
-#     set.seed(RNG_seed)
-#     params %>% 
-#         grid_latin_hypercube(size = size)
-# }
-# 
-# tune_xgb_grid <- function(wflow, 
-#                           resamples, 
-#                           params, 
-#                           grid) {
-#     tune_grid(
-#         wflow, 
-#         resamples = resamples, 
-#         param_info = params, 
-#         grid = grid, 
-#         metrics = metric_set(roc_auc), 
-#         control = control_grid(
-#             verbose = TRUE, 
-#             allow_par = TRUE, 
-#             save_pred = TRUE
-#         )
-#     )
-# }
-# 
-# tune_xgb_bayes <- function(wflow, 
-#                            resamples, 
-#                            params, 
-#                            grid_results, 
-#                            iter, 
-#                            RNG_seed) {
-#     tune_bayes(
-#         wflow, 
-#         resamples = resamples, 
-#         iter = iter, 
-#         param_info = params, 
-#         metrics = metric_set(roc_auc), 
-#         initial = grid_results, 
-#         control = control_bayes(
-#             verbose = TRUE, 
-#             seed = RNG_seed, 
-#             save_pred = TRUE
-#         )
-#     )
-# }
+create_xgb_grid <- function(params,
+                            size,
+                            RNG_seed) {
+    set.seed(RNG_seed)
+    params %>%
+        grid_max_entropy(size = size) %>% 
+        mutate(sample_size = as.double(sample_size))
+}
+
+tune_xgb_grid <- function(wflow,
+                          resamples, 
+                          grid, 
+                          params) {
+    tune_grid(
+        wflow,
+        resamples = resamples,
+        grid = grid,
+        param_info = params,
+        metrics = metric_set(roc_auc),
+        control = control_grid(
+            verbose = TRUE,
+            save_pred = TRUE
+        )
+    )
+}
+
+tune_xgb_bayes <- function(wflow,
+                           resamples,
+                           iter, 
+                           params,
+                           grid_results) {
+    tune_bayes(
+        wflow,
+        resamples = resamples,
+        iter = iter,
+        param_info = params,
+        metrics = metric_set(roc_auc),
+        initial = grid_results,
+        control = control_bayes(
+            verbose = TRUE,
+            save_pred = TRUE
+        )
+    )
+}
