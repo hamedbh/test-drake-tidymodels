a931db6fba96fb56c23f7e7ebab0ae5245f14dc3
hamedbh <hamedbh@users.noreply.github.com>
2020-04-16 13:40:47 +0100

    add specific plotting functions for EDA

--- R/f_plotting.R
+++ R/f_plotting.R
@@ -11,17 +11,66 @@ plot_barplot_grid <- function(d) {
         geom_bar() +
         facet_wrap(~ feature, scales = "free_x") +
         theme_minimal() +
-        theme(axis.text.x = element_blank())
+        theme(axis.text.x = element_blank()) + 
+        labs(
+            title = "Distribution of categorical variables", 
+            x = NULL
+        )
 }
 
-
 plot_density_grid <- function(d) {
-    # then histograms for integers
     d %>%
         dplyr::select_if(is.integer) %>%
+        dplyr::select(-c(num_dependents, 
+                         num_existing_credits, 
+                         pct_of_income, 
+                         resident_since)) %>% 
         tidyr::gather(key = "predictor") %>%
         ggplot(aes(x = value)) +
         geom_density() +
         facet_wrap(~ predictor, scales = "free") +
-        theme_minimal()
+        theme_minimal() + 
+        theme(axis.text.y = element_blank()) + 
+        labs(
+            title = "Distribution of continuous variables", 
+            x = NULL
+        )
+}
+
+plot_log_amount <- function(d) {
+    d %>% 
+        ggplot(aes(x = amount)) + 
+        geom_density() + 
+        theme_minimal() + 
+        scale_x_log10(labels = function(x) {
+            scales::dollar(x,
+                           accuracy = 1, 
+                           prefix = "€")
+        }) + 
+        theme(axis.text.y = element_blank()) + 
+        labs(
+            title = expression(paste("Distribution of loan amount, ", 
+                                     log[10], 
+                                     " scale", 
+                                     sep = "")), 
+            x = NULL
+        )
+}
+
+plot_integer_grid <- function(d) {
+    d %>% 
+        dplyr::select(c(num_dependents, 
+                        num_existing_credits, 
+                        pct_of_income, 
+                        resident_since)) %>% 
+        tidyr::gather(key = "predictor") %>%
+        mutate_all(factor) %>% 
+        ggplot(aes(x = value)) +
+        geom_bar() +
+        facet_wrap(~ predictor, scales = "free") +
+        theme_minimal() + 
+        labs(
+            title = "Distribution of integer variables", 
+            x = NULL
+        )
 }
